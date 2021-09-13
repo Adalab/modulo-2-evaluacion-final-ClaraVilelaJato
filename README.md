046bd586c9208330c4cbf4d53bedcaa31ce5cc72
Clara Vilela <claravilelajato@gmail.com>
2021-09-13 10:35:21 +0200

    Comment code

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 ![Adalab](https://beta.adalab.es/resources/images/adalab-logo-155x61-bg-white.png)
 
-# Adalab web starter kit
+Ahoy! Este es mi trabajo de evaluación del modulo 2 del curso de desarroloweb de Adalab creado con el Starter kit proporcionado por la escuela, que utiliza en **node y gulp**. ¿Y qué es un Starter kit? Pues es una **plantilla de proyecto con funcionalidades preinstaladas y preconfiguradas**.
 
-Ahoy! Este es nuestro Starter Kit creado en **node y gulp**. ¿Y qué es un Starter kit? Pues es una **plantilla de proyecto con funcionalidades preinstaladas y preconfiguradas**.
-
-Este Kit incluye un motor de plantillas HTML, el preprocesador SASS y un servidor local y muchas cosas más. El Kit nos ayuda a trabajar más cómodamente, nos automatiza tareas.
+Este Kit incluye un motor de plantillas HTML, el preprocesador SASS y un servidor local y muchas cosas más.
 
 En el Kit hay 3 tipos de ficheros y carpetas:
 
@@ -12,20 +10,15 @@ En el Kit hay 3 tipos de ficheros y carpetas:
 - La carpeta `src/`: son los ficheros de nuestra página web, como HTML, CSS, JS...
 - Las carpetas `public/` y `docs/`, que son generadas automáticamente cuando arrancamos el proyecto. El Kit lee los ficheros que hay dentro de `src/`, los procesa y los genera dentro de `public/` y `docs/`.
 
+Además he incluído un desglose de tareas que me han ayudado a afrontar en proyecto global en pequeños hitos.
+
 ## Guía de inicio rápido
 
 > **NOTA:** Necesitas tener instalado [Node JS](https://nodejs.org/) para trabajar con este Starter Kit:
 
-### Pasos a seguir cada vez que queremos arrancar un proyecto desde cero:
+### Pasos a seguir para arrancar mi proyecto:
 
-1. **Crea tu propio repositorio.**
-1. Descarga este **Starter kit desde GitHub**.
-   - No recomendamos que clones este repo ya que no podrás añadir commits.
-1. **Copia todos los ficheros** de este Starter kit en la carpeta raíz de tu repositorio.
-   - Recuerda que debes copiar **también los ficheros ocultos**.
-   - Si has decidido clonar este repo, no debes copiar la carpeta `.git`. Si lo haces estarás machacando tu propio repositorio.
-1. **Abre una terminal** en la carpeta raíz de tu repositorio.
-1. **Instala las dependencias** locales ejecutando en la terminal el comando:
+### Instala las dependencias:
 
 ```bash
 npm install
@@ -44,8 +37,8 @@ Este comando:
 - **Abre una ventana de Chrome y muestra tu página web**, al igual que hace el plugin de VS Code Live Server (Go live).
 - También **observa** todos los ficheros que hay dentro de la carpeta `src/`, para que cada vez que modifiques un fichero **refresca tu página en Chrome**.
 - También **procesa los ficheros** HTML, SASS / CSS y JS y los **genera y guarda en la carpeta `public/`**. Por ejemplo:
-   - Convierte los ficheros SASS en CSS.
-   - Combina los diferentes ficheros de HTML y los agrupa en uno o varios ficheros HTML.
+  - Convierte los ficheros SASS en CSS.
+  - Combina los diferentes ficheros de HTML y los agrupa en uno o varios ficheros HTML.
 
 Después de ejecutar `npm start` ya puedes empezar a editar todos los ficheros que están dentro de la carpeta `src/` y programar cómodamente.
 
@@ -69,6 +62,7 @@ Además, los comandos:
 ```bash
 npm run push-docs
 ```
+
 o
 
 ```bash
@@ -110,14 +104,6 @@ src
  └─ html
     └─ partials
 ```
-
-> **NOTA:** Los partials de HTML y SASS del proyecto son orientativos. Te recomendamos usar los que quieras, y borrar los que no uses.
-
-## Vídeotutoriales del Starter kit
-
-- [Qué es, trabajar con la versión de desarrollo y rutas relativas](https://www.youtube.com/watch?v=XwvhXvBijos)
-- [Migración de un proyecto, trabajar con la versión de producción y GitHub Pages](https://www.youtube.com/watch?v=qqGClcgt9Uc)
-- [Motor de plantillas](https://www.youtube.com/watch?v=4GwXOJ045Zg)
 
 ## Falta algo?
 
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,16 +6,9 @@ const searchButton = document.querySelector('.js-search-btn');
 const reset = document.querySelector('.reset__btn');
 const resultsContainer = document.querySelector('.js-list-results'); //selecciono el elem. de html donde pintaré los resultados
 const paintedFavs = document.querySelector('.js-favs');
-let apiDataShows = []; //array donde almaceno resultados
+let apiDataShows = []; //array donde almaceno resultados del api
 
-let favorites = [];
-
-function setInLocalStorage() {
-  // stringify me permite transformar a string el array de favorites
-  const stringShows = JSON.stringify(favorites);
-  //añadimos  al localStorage  los datos convertidos en string previamente
-  localStorage.setItem('favorites', stringShows);
-}
+let favorites = []; //array donde almaceno favoritos
 
 //Función para hacer petición al servidor
 
@@ -24,21 +17,28 @@ function fetchToApi() {
   fetch(`https://api.tvmaze.com/search/shows?q=${inputShowValue}`)
     .then((response) => response.json())
     .then((data) => {
-      apiDataShows = data;
+      apiDataShows = data; //guardo los datos del api en el array global
     });
-  paintShows();
+  paintShows(); //incluyo la funcion painShows para que se me pinten los datos en cada petición
+}
+
+//Declaro la función para guardar en localStorage
+function setInLocalStorage() {
+  // stringify me permite transformar a string el array de favorites
+  const stringShows = JSON.stringify(favorites);
+  //añadimos  al localStorage  los datos convertidos en string previamente
+  localStorage.setItem('favorites', stringShows);
 }
 
 function getLocalStorage() {
-  // obtenermos lo que hay en el LS
+  // obtengo lo que hay en el LS
   const localStorageLoaded = localStorage.getItem('favorites');
-  // siempre que cojo datos del local storage tengo que comprobar si son válidos
-  // es decir si es la primera vez que entro en la página
+  // cojo datos del local storage y compruebo si son válidos
   if (localStorageLoaded === null) {
     // no tengo datos en el local storage, así que llamo al API
     fetchToApi();
   } else {
-    // sí tengo datos en el local storage,lo parseo a un array y
+    //tengo datos en el local storage,lo parseo a un array y
     const arrayFavorites = JSON.parse(localStorageLoaded);
     // lo guardo en la variable global de favorites
     favorites = arrayFavorites;
@@ -47,23 +47,26 @@ function getLocalStorage() {
   }
 }
 
+//declaro la función manejadora del evento click que está asociado al botón search
 function handleSearch(ev) {
   ev.preventDefault();
   fetchToApi();
 }
-
 //Añadimos evento que desencadena la funcion fetch al pulsar el boton, nos traemos los datos del api
 searchButton.addEventListener('click', handleSearch);
 
 // 1- -- Cuando carga la pagina
 getLocalStorage();
 
-//Esta función es la encargada de manejar el evento click sobre cada uno de los li
+//Esta función es la encargada de manejar el evento click (listenShows) sobre cada uno de los li de paintShows
 function handleShows(ev) {
-  const pickedShow = parseInt(ev.currentTarget.id);
+  const pickedShow = parseInt(ev.currentTarget.id); //los datos del id que recogemos de html son un string y los que recogemos del api number, así que los convierto a number con parseInt
+
+  //a continuación aplico el metodo find al array apidatashows, de cada show le digo que me encuentre su id y comparo si es el mismo que el de html
   const objectClickedShow = apiDataShows.find((show) => {
     return show.show.id === pickedShow;
   });
+  //del array de favoritos busco su posición en el array
   const favoritesFound = favorites.findIndex((fav) => {
     return fav.show.id === pickedShow;
   });
@@ -78,41 +81,44 @@ function handleShows(ev) {
     // y quiero borrar un solo elemento por eso colocamos 1
     favorites.splice(favoritesFound, 1);
   }
-  console.log(favorites);
+  //console.log(favorites);
   paintShows();
   paintFavs();
   setInLocalStorage();
 }
 
+function listenShows() {
+  const showList = document.querySelectorAll('.js-picked'); //selecciono los li que creé con la funcion paintShows, a continuación los recorro con un bucle y añado el tipo de evento y la función manejadora cuando el evento se desencadene.
+  for (const show of showList) {
+    show.addEventListener('click', handleShows);
+  }
+}
+
+//Declaro una función para manejar el evento click sobre las X del listado de favoritos
+//el funcionamiento es muy similar al de la función handleShows, usando el metodo findIndex para saber si en el array está el elemento que tenga el mismo id que el elemento que estamos escuchando cen el evento click.
 function handleX(ev) {
   const showToClose = parseInt(ev.currentTarget.id);
   const favoritesFound = favorites.findIndex((fav) => {
     return fav.show.id === showToClose;
   });
   if (favoritesFound === -1) {
+    //lo añado, y lo quito
     favorites.splice(favoritesFound, 1);
   }
   paintFavs();
   paintShows();
   setInLocalStorage();
 }
-
+//declaro esta funcion para añadir cuando se pinten los favoritos, manejada por la función handleX
 function closeX() {
   const listX = document.querySelectorAll('.close-fav');
   for (let x of listX) {
     x.addEventListener('click', handleX);
-    console.log(listX);
+    //console.log(listX);
   }
 }
 
-function listenShows() {
-  const showList = document.querySelectorAll('.js-picked'); //le doy clase a los li que cree con la funcion paint
-  for (const show of showList) {
-    show.addEventListener('click', handleShows);
-  }
-}
-
-//codigo botón de reset
+//código botón de reset, me vacías el array y el html de los elementos con clase .js-favs, limpias el localStorage con el método clear
 
 function handleresetFavs() {
   favorites = [];
@@ -120,15 +126,15 @@ function handleresetFavs() {
   localStorage.clear();
 }
 
-reset.addEventListener('click', handleresetFavs);
+reset.addEventListener('click', handleresetFavs); //añado el evento click al botón
 
-//esta funcion la incluimos en la funcion fecth
+//esta funcion la incluimos en la funcion fecth, cuando recibimos los datos solicitados los pintamos en el html
 function paintShows() {
   let html = '';
   let favClass = '';
   for (const eachSerie of apiDataShows) {
     const oneSerie = eachSerie.show;
-    const isFav = isFavorite(oneSerie);
+    const isFav = isFavorite(oneSerie); //guardo esta funcion (declarada en la linea 181) con oneSerie como parámetro dentro de la constante isfav. Si es true añado la clase, si no vacio, para usarla al pintar los li y darle un css diferente.
     if (isFav) {
       favClass = 'js-selected-list';
     } else {
@@ -170,7 +176,7 @@ function paintFavs() {
   closeX();
 }
 
-//funcion para añadir una clase a los favoritos que se encuentren el array cuando utilizo paintShows
+//funcion para añadir una clase para dar estilos diferentes a los favoritos que se encuentren el array favorites cuando utilizo paintShows
 
 function isFavorite(oneSerie) {
   const favoriteFound = favorites.find((fav) => {
